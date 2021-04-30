f0a8b15fd689cc8fb11e50fb6ae1559bc34d7041
NikolayLatsyn <nikolay.lastyn@firstlinesoftware.com>
2021-04-30 12:06:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// var product = 'Socks'
+app  = new Vue({
+    el: '#app',
+    data: { 
+        product: {
+            name:'Socks',
+            description: 'A pair of warm, fuzzy socks',
+            image: './assets/vmSocks-green-onWhite.jpg',
+            link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+            altText: 'image of socks',
+            linkText: 'More products'
+        }
+    }
+})
