6dd2a85a5a83e38bdbf6dce9f3a45f864b59d023
LatsynNik <latsynn@mail.ru>
2021-04-30 19:07:53 +0300

    Add computed props

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ app  = new Vue({
     data: { 
         name:'Socks',
         description: 'A pair of warm, fuzzy socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
+        brand: 'My Vue Brand',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         altText: 'image of socks',
         linkText: 'More products',
         inventory: 100,
-        inStock: true,
         onSale: true,
+        selectedVariantIndex: 0,
         details: [
                 "1 detail",
                 "second",
@@ -21,12 +21,14 @@ app  = new Vue({
                 {
                     id: 2201,
                     color: "green",
-                    image: "assets/vmSocks-green-onWhite.jpg" 
+                    image: "assets/vmSocks-green-onWhite.jpg",
+                    inventory: 5
                 },
                 {
                     id: 2202,
                     color: "blue",
-                    image: "assets/vmSocks-blue-onWhite.jpg"
+                    image: "assets/vmSocks-blue-onWhite.jpg",
+                    inventory: 0
                 }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -44,8 +46,23 @@ app  = new Vue({
             this.cart -= 1
         },
 
-        updateImage(variant){
-            this.image = variant.image
+        updateVariant(variantIndex){
+            this.selectedVariantIndex = variantIndex
+        }
+    },
+    computed:{
+        image(){
+            return this.variants[this.selectedVariantIndex].image
+        },
+        title(){
+            return this.name + ' ' + this.brand
+        },
+        inStock(){
+            return this.variants[this.selectedVariantIndex].inventory > 0
+        },
+        saleText(){
+            var text = `Hurry to get this ${this.name} from ${this.brand}`
+            return this.onSale ? text+' on sale!' : text
         }
     }
 })
